f7c226f511b401d1cc89d7d5cedebb529573a660
Ubuntu <kraiwitu@vm-AirflowTraining-018.fpbomhon4b5utd3f5yw4sbx20g.ix.internal.cloudapp.net>
2024-11-19 08:37:18 +0000

    Add initial project setup and configuration

---
+++ dags/umbrella.py
@@ -0,0 +1,29 @@
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+
+#g are defaults which can be overridden later on
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 11, 18),
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG('umbrella', default_args=default_args)
+
+
+fetch_weather_forecast = BashOperator(task_id="fetch_weather_forecast",bash_command='echo "fetch_weatger_forecast"', dag=dag)
+fetch_sales_data = BashOperator(task_id="fetch_sales_data",bash_command='echo "fetch_sales_data"', dag=dag)
+clean_forecast_data = BashOperator(task_id="clean_forecast_data",bash_command='echo "fetch_weatger_forecast"', dag=dag)
+join_datasets = BashOperator(task_id="join_datasets",bash_command='echo "fetch_weatger_forecast"', dag=dag)
+
+# Set dependencies between all tasks
+fetch_weather_forecast >> clean_forecast_data >> join_datasets
+
+
